48069da705f244b499904345d1c7d709823039f5
iceCreamCoder <jared.web@live.com>
2019-11-09 17:32:21 -0800

    adding app structure

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav style={{display: flex}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
