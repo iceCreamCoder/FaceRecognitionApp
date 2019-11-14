573f7626eb7427e7de20ae505a03b601d3622d0b
iceCreamCoder <jared.web@live.com>
2019-11-14 08:17:58 -0800

    added gh-pages

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -6,7 +6,7 @@ const SignIn = ({onRouteChange}) => {
     return (
         <article className="br3 shadow-4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 
                 <legend className="f1 fw6 ph0 mh0">Sign In</legend>
@@ -40,7 +40,7 @@ const SignIn = ({onRouteChange}) => {
                 </p>
                 
                 </div>
-            </form>
+            </div>
         </main>
         </article>
     )
