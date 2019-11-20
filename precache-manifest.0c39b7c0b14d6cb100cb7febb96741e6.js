d29fe53bfd425febd5f9acb1f03101d577e9686b
iceCreamCoder <jared.web@live.com>
2019-11-20 07:48:50 -0800

    Updates

---
+++ precache-manifest.0c39b7c0b14d6cb100cb7febb96741e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af1bb2206a91bbb7bc352aca65175c4",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "394842b73f7be220ba8e",
+    "url": "/FaceRecognitionApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7e3207b137cf32833a18",
+    "url": "/FaceRecognitionApp/static/css/main.21289b47.chunk.css"
+  },
+  {
+    "revision": "394842b73f7be220ba8e",
+    "url": "/FaceRecognitionApp/static/js/2.747c1a25.chunk.js"
+  },
+  {
+    "revision": "7e3207b137cf32833a18",
+    "url": "/FaceRecognitionApp/static/js/main.b5592697.chunk.js"
+  },
+  {
+    "revision": "7b2226a6f0ed18ff37cd",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.24c09067.js"
+  }
+]);
